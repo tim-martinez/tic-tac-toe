d4cd8cd39953acce601c4285ab5f489b38415b02
Tim Martinez <83369306+timperry151@users.noreply.github.com>
2023-11-28 05:18:20 -0500

    fixed logic for win condition.

--- main.js
+++ main.js
@@ -24,20 +24,40 @@ const createPlayer = function (symbol) {
   return { player, playerScore, computer, computerScore };
 };
 
-//default player selects x
-const players = createPlayer('x');
-console.log(players);
+//player selects symbol
+const playerSelect = (function () {
+  const x = document.querySelector('.x');
+  const o = document.querySelector('.o');
 
-//play 1 round of tic tac toe
-const playGame = function (row, col) {
-  //declare game board
-  console.log(gameBoard);
+  o.addEventListener('click', function () {
+    const players = createPlayer('o');
+    console.log(`selected ${players.player}`);
+    renderGameBoard();
+    selectBox(players);
+  });
 
+  x.addEventListener('click', function () {
+    const players = createPlayer('x');
+    console.log(`selected ${players.player}`);
+    renderGameBoard();
+    selectBox(players);
+  });
+})();
+
+//play round of tic tac toe
+const playGame = function (row, col, players) {
   //player one selects square
   if (gameBoard[row][col] === '') {
     gameBoard[row][col] = players.player;
   } else {
     return 'choose another square';
+  }
+
+  if (checkWin() === true) {
+    console.log('winner stop game! - 1st check');
+    // checkWin();
+    // renderGameBoard();
+    return true;
   }
 
   //declare random indices
@@ -63,7 +83,12 @@ const playGame = function (row, col) {
     gameBoard[randomRow][randomColumn] = players.computer;
   }
 
-  checkWin();
+  if (checkWin() === true) {
+    // checkWin();
+    console.log('winner stop game!  - 2nd check');
+    renderGameBoard();
+    return true;
+  }
   console.log(checkWin());
   const container = document.querySelector('.container');
 
@@ -72,8 +97,13 @@ const playGame = function (row, col) {
     container.removeChild(container.firstChild);
   }
   renderGameBoard();
+  if (checkWin() === true) {
+    return;
+  }
+  selectBox(players);
 };
 
+//check if there is a winner
 function checkWin() {
   // Check rows and columns
   for (let i = 0; i < 3; i++) {
@@ -82,14 +112,20 @@ function checkWin() {
       gameBoard[i][0] === gameBoard[i][1] &&
       gameBoard[i][0] === gameBoard[i][2]
     ) {
-      return `${gameBoard[i][0]} wins!`; // Row win
+      const winner = gameBoard[i][0];
+      renderGameBoard();
+      declareWinner(winner); // Row win
+      return true;
     }
     if (
       gameBoard[0][i] !== '' &&
       gameBoard[0][i] === gameBoard[1][i] &&
       gameBoard[0][i] === gameBoard[2][i]
     ) {
-      return `${gameBoard[0][i]} wins!`; // Column win
+      const winner = gameBoard[0][i];
+      renderGameBoard();
+      declareWinner(winner); // column win
+      return true;
     }
   }
 
@@ -99,31 +135,68 @@ function checkWin() {
     gameBoard[0][0] === gameBoard[1][1] &&
     gameBoard[0][0] === gameBoard[2][2]
   ) {
-    return `${gameBoard[0][0]} wins!`; // Diagonal from top-left to bottom-right
+    const winner = gameBoard[0][0];
+    renderGameBoard();
+    declareWinner(winner);
+    return true; // Diagonal from top-left to bottom-right
   }
   if (
     gameBoard[0][2] !== '' &&
     gameBoard[0][2] === gameBoard[1][1] &&
     gameBoard[0][2] === gameBoard[2][0]
   ) {
-    return `${gameBoard[0][2]} wins!`; // Diagonal from top-right to bottom-left
+    const winner = gameBoard[0][2];
+    renderGameBoard();
+    declareWinner(winner);
+    return true; // Diagonal from top-right to bottom-left
   }
 
-  return 'no win'; // No win
+  return false; // No win
 }
 
+//display the game board
 const renderGameBoard = function () {
   const container = document.querySelector('.container');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  container.classList.add('gameboard');
 
   gameBoard.forEach((row, rowIndex) => {
     row.forEach((cell, colIndex) => {
       const tile = document.createElement('div');
       tile.classList.add('tile');
-      tile.dataset.index = `${rowIndex}-${colIndex}`;
+      tile.dataset.row = `${rowIndex}`;
+      tile.dataset.col = `${colIndex}`;
       tile.innerText = cell;
       container.append(tile);
     });
   });
 };
 
-playGame(1, 1);
+//click events for squares
+const selectBox = function (players) {
+  const tile = document.querySelectorAll('.tile');
+
+  tile.forEach(function (element) {
+    element.addEventListener('click', function () {
+      const row = element.dataset.row;
+      const col = element.dataset.col;
+      console.log(row + col);
+      playGame(row, col, players);
+    });
+  });
+};
+
+//declare winner
+
+const declareWinner = function (winner) {
+  const body = document.body;
+  const div = document.createElement('div');
+  const winnerText = document.createElement('h2');
+  winnerText.textContent = `${winner} wins!`;
+
+  div.classList = 'winner';
+  div.append(winnerText);
+  body.insertBefore(div, body.firstChild);
+};
